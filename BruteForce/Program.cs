12cd11359c26ef16ce9d6e8623512ccb5031009f
Michiel Van Eester <michiel.vaneester@student.ap.be>
2019-03-26 13:07:34 +0100

    Bruteforce working with chars instead of string

--- BruteForce/Program.cs
+++ BruteForce/Program.cs
@@ -7,9 +7,9 @@ namespace BruteForce
     {
         static void Main(string[] args)
         {
-            String[] arrayChars = new string[63] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s" ,"t" ,"u" , "v", "w", "x", "y", "z",
-                                                    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-                                                    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " " };
+            Char[] arrayChars = new char[63] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's' ,'t' ,'u', 'v', 'w', 'x', 'y', 'z',
+                                                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                                                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ' };
 
 
             bool match = false;
@@ -45,7 +45,7 @@ namespace BruteForce
                     for (int c = 0; c < arrayChars.Length; c++)
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
-                        currentChar = arrayChars[c];
+                        currentChar = Convert.ToString(arrayChars[c]);
 
                         if (currentChar == Convert.ToString(arrayPwd[i]))
                         {
